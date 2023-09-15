9056bfcfba5a0fe9793fa51b63548ce679a67a72
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-15 16:04:23 +0200

    JavaScript try

--- public/script.js
+++ public/script.js
@@ -1,38 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("vegetableForm");
-  //   const sowedCheckbox = document.getElementById("sowed");
-  //   const plantedCheckbox = document.getElementById("planted");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent the default form submission
+document.addEventListener("DOMContentLoaded", function(){
+    const sensorValueLeftElement = document.getElementById('sensor_value_left');
+    const soil_humidity_left_gauge = document.getElementById('soil_humidity_left_gauge');
+    
+    // Define the API URL you want to fetch data from
+    const apiUrl = 'http://192.168.1.104:5001/api/sensors/last';
 
-    // Gather form data and create a JSON object
-    const formData = new FormData(form);
-    const jsonData = {};
-    // formData.set("sowed", sowed.checked);
-    // formData.set("planted", planted.checked);
-    formData.forEach((value, key) => {
-      jsonData[key] = value;
-    });
-    console.log(jsonData);
-    // Send the JSON data to your server
-    const apiUrl = "http://192.168.1.101:5001/api/v1/vegetable_manager"; // Replace with your API endpoint URL
+    // Fetch data from the API
+    fetch(apiUrl)
+        .then(response => response.json()) // Assuming the API returns JSON data
+        .then(data => {
+           // Extract the value of soil_humidity_1 from the data object
+            const soilHumidity1Value = data.soil_humidity_1;
+            // Update the content of the element with the extracted value
+            sensorValueLeftElement.textContent = `${soilHumidity1Value}`;
+            // Determine which image to display based on the value
+            let imageSrc;
+            if (soilHumidity1Value < 2500) {
+                imageSrc = './assets/icon_gauge.png'; // Use the high value image
+            } else {
+                imageSrc = './assets/icon_humidity.png';  // Use the low value image
+            }
 
-    fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(jsonData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response from the server if needed
-        console.log("Response from server:", data);
-        // Reset the form
-        form.reset();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  });
+            // Update the image source
+            soil_humidity_left_gauge.src = imageSrc;
+
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 });
