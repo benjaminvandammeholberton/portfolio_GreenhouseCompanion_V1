e99bfd638dbb4b39a928b350cadc9ea0b860cbde
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-16 20:31:31 +0200

    Add feature: vegetable general infos + routes

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -6,3 +6,4 @@ from api.views.vegetable_manager import *
 from api.views.garden_area import *
 from api.views.sensors import *
 from api.views.to_do_list import *
+from api.views.vegetable_infos import *
--- models/vegetable_infos.py
+++ models/vegetable_infos.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 """ holds class VegetableInfos"""
 import models
 from models.base_model import BaseModel, Base
